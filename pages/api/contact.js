e1173800381b4660d2434592738beeeae94ad25c
RMDcode <dhurir163@gmail.com>
2024-06-04 14:21:06 +0530

    nodemailer update

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -18,6 +18,17 @@ const handler = async (req, res) => {
         text: `Name: ${data.name}\nEmail: ${data.email}\nPhone: ${data.phone}\n\nMessage:\n${data.message}`,
         html: `<h1>${data.subject}</h1><p>Username: ${data.name}<br> Useremail: ${data.email}<br> Phone: ${data.phone}<br>Message:<br>${data.message}</p>`,
       });
+      
+      await transporter.sendMail({
+        ...mailOptions,
+        to: data.email, // You can set this dynamically
+        cc:"dhurir163@gmail.com",
+        subject: "Message has been sent to Rohit Dhuri",
+        text: `Name: ${data.name}\nEmail: ${data.email}\nPhone: ${data.phone}\n\nMessage:\n${data.message}`,
+        html: `<h1>Hi ${data.name},</h1><br><p>Thanks for reach out to me.</p><p>I will reach out to you for ${data.subject}.</p><br><br><br>
+        <p>Regards,</p><h2>Rohit Dhuri</h2>`,
+      });
+
       return res.status(200).json({ success: true });
     } catch (error) {
       console.log('Error sending email:', error);
