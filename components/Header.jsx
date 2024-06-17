d380c4ff111d5b4b8974ef16b511ca4a22bddd48
RMDcode <dhurir163@gmail.com>
2024-06-18 00:50:56 +0530

    Updated hireme

--- components/Header.jsx
+++ components/Header.jsx
@@ -19,12 +19,12 @@ const Header= ()=> {
 
 
       {/* Desktop Nav & hire me button */}
-      {/*<div className="hidden xl:flex items-center gap-8">
+      <div className="hidden xl:flex items-center gap-8">
       <Nav/>
-      <Link href="/contact">
+      {/*<Link href="/contact">
         <Button>Hire me</Button>
-      </Link>
-      </div>*/}
+      </Link>*/}
+      </div>
       
       {/* Mobile Nav */}
       <div className="xl:hidden">
